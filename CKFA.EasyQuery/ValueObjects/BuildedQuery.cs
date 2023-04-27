5e6b270b832be57a4fdeb53c30dbc8f412f34d19
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-04-27 14:12:38 -0300

    feat: implemented new class as return from queryBuilder

---
+++ CKFA.EasyQuery/ValueObjects/BuildedQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace CKFA.EasyQuery.ValueObjects
+{
+    public class BuildedQuery
+    {
+        public string Query { get; set; }
+        public DynamicParameters Parameters { get; set; }
+    }
+}
